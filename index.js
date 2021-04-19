f2424a5d359a050735bc563af226fda884597061
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-19 11:44:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let userId = "tharunece95@gmail.com";
-let url = `https://gmail.googleapis.com/gmail/v1/users/${userId}/messages`;
+let url = `https://mail.google.com/https://gmail.googleapis.com/gmail/v1/users/${userId}/messages`;
 
 async function getData() {
   let res = await fetch(url);
