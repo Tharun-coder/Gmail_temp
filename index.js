3fb42e0212a07f2d686302de8d833d23ec0ca62c
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-19 13:39:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 let userId = "tharunece95@gmail.com";
 let url = `https://gmail.googleapis.com/gmail/v1/users/${userId}/messages`;
 let scope = "https://mail.google.com/";
+let client_id =
+  "483228225913-7tmuhk2cicdd85nbo4nboaujmn6lr0sk.apps.googleusercontent.com";
 
 let token_url = "https://accounts.google.com/o/oauth2/token";
 
 async function getToken() {
+  let res = await fetch(`https://accounts.google.com/o/oauth2/v2/auth?
+    scope=https//www.googleapis.com/&
+    include_granted_scopes=true&
+    response_type=token&
+    state=state_parameter_passthrough_value&
+    redirect_uri=https://tharun-coder.github.io/Gmail_temp/&
+    client_id=${client_id}`);
+  let data = res.json();
+  console.log(data);
+}
+
+getToken();
+
+async function getData() {
   let res = await fetch(
     url +
       `?access_token=ya29.a0AfH6SMAahj-HXMdv-NmCEbKAem5mS8swb6hkuUydmG4yiv1Kn9wCQ9CeFw3jNj-gsh-oLx6INE6VrOXw1PjJvaMPaFV3lU2qobbwYy2gy9cO-oUMOtusMWjQmjWiZwr0v4M-fUYpSloUU8CvWsEdqfEg84Iq`
   );
   let data = await res.json();
-  console.log(data);
+  //   console.log(data);
 }
 
-getToken();
+// getToken();
 
 //https://tharun-coder.github.io/Gmail_temp/#state=try_sample_request&
 //access_token=ya29.a0AfH6SMAahj-HXMdv-NmCEbKAem5mS8swb6hkuUydmG4yiv1Kn9wCQ9CeFw3jNj-gsh-oLx6INE6VrOXw1PjJvaMPaFV3lU2qobbwYy2gy9cO-oUMOtusMWjQmjWiZwr0v4M-fUYpSloUU8CvWsEdqfEg84Iq&
