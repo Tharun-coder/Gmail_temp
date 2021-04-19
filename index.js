ae47aa8a5da2c0f57c15c0f2d27d0751a29bf609
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-19 16:15:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,19 +26,24 @@ async function getData() {
       `?access_token=ya2.a90AfH6SMBfrIKm3EQy0tK7y8HIeErf-uzDI2s3mDC-ADK04Gb99iFsvGB--WcY6iMaDQkV2KKZeKABTSSPFg-YYGqmXlBmZrW2ipo-HeeXgfDhZ_TWDeibkgG99KodQl-spq0go9yKrhJ5DIT0ZZcRufrCZTcg`
   );
   let data = await res.json();
-  let messages = data.forEach((e)=>{
-      let msg = await fetch(url`/${e.id}` +
-        `?access_token=ya2.a90AfH6SMBfrIKm3EQy0tK7y8HIeErf-uzDI2s3mDC-ADK04Gb99iFsvGB--WcY6iMaDQkV2KKZeKABTSSPFg-YYGqmXlBmZrW2ipo-HeeXgfDhZ_TWDeibkgG99KodQl-spq0go9yKrhJ5DIT0ZZcRufrCZTcg`)
-        let msg_data = await msg.json()
-        return msg_data
-    })
+  let messages = [];
+  messages.push(data.forEach((e) => getMessages(e.id)));
 
   console.log(data);
-  console.log(messages)
-
+  console.log(messages);
 }
 
 getData();
+
+async function getMessages(id) {
+  let msg = await fetch(
+    url`/${id}` +
+      `?access_token=ya2.a90AfH6SMBfrIKm3EQy0tK7y8HIeErf-uzDI2s3mDC-ADK04Gb99iFsvGB--WcY6iMaDQkV2KKZeKABTSSPFg-YYGqmXlBmZrW2ipo-HeeXgfDhZ_TWDeibkgG99KodQl-spq0go9yKrhJ5DIT0ZZcRufrCZTcg`
+  );
+  let msg_data = await msg.json();
+  console.log(msg_data);
+}
+
 
 //https://tharun-coder.github.io/Gmail_temp/#state=try_sample_request&
 //access_token=ya29.a0AfH6SMBfrIKm3EQy0tK7y8HIeErf-uzDI2s3mDC-ADK04Gb99iFsvGB--WcY6iMaDQkV2KKZeKABTSSPFg-YYGqmXlBmZrW2ipo-HeeXgfDhZ_TWDeibkgG99KodQl-spq0go9yKrhJ5DIT0ZZcRufrCZTcg&
