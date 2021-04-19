14397906b1fb36ca6c764b06d4eba93911448c06
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-19 19:30:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ async function getToken() {
   console.log(res);
 }
 
-getToken();
+// getToken();
 
 async function getData() {
   try{
   let res = await fetch(
     url +
-      `/178e9d13d2acc926?access_token=ya29.a0AfH6SMDorYegsTRBbKeQZxpg2-g45XY1w2Z1u2Q2mKQ_BydpbnPDc44QyWoKQUr6pRzmz9cOFgDvY-8tPYV5yQ6ppwN6pBhs2820_izmO9t-qxuRLO0bg5_PQu8tEiV6aHEK98CSzO0GWfZjVJPSjkkLu-oJ`
+      `/178e9d13d2acc926?access_token=ya29.a0AfH6SMB6NuYpIwZ-Tcv0k3Q1zx1pQ1El_GyJ2D62_JH-vFZ-j34q1xPSdnD9-vqxWNbSMkeEgLdk6qkhRYeAjA1iDg7ntAox_B2zLtzN9qGkn6_OYZ69HcTsbfVVDFjPFtvx4vVXt7BEBkaoGOPSF55QIA78Nw`
   );
   let data = await res.json();
   console.log(data);
