630df4435301dc6348e2b30985b585d9a6b9e6f5
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-19 11:16:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let userId = "tharunece95@gmail.com";
+let url = `https://gmail.googleapis.com/gmail/v1/users/${userId}/messages`;
+
+async function getData() {
+  let res = await fetch(url);
+  let data = await res.json();
+  console.log(data);
+}
