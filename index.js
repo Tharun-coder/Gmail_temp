c4310229224b6e29a979d172b7eb136552d99c0a
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-20 11:37:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ let token_url = "https://accounts.google.com/o/oauth2/token";
 function onSignIn(googleUser) {
         // Useful data for your client-side scripts:
         var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log("Full Name: " + profile.getName());
-        console.log("Given Name: " + profile.getGivenName());
-        console.log("Family Name: " + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
+//         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+//         console.log("Full Name: " + profile.getName());
+//         console.log("Given Name: " + profile.getGivenName());
+//         console.log("Family Name: " + profile.getFamilyName());
+//         console.log("Image URL: " + profile.getImageUrl());
+//         console.log("Email: " + profile.getEmail());
 
         // The ID token you need to pass to your backend:
         var access_token = googleUser.getAuthResponse(true).access_token;
